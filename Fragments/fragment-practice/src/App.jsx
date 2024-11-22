11b501b139798541e2e9328d117901729a4107eb
sidz111 <sssurwade2212@gmail.com>
2024-11-22 13:46:07 +0530

    updates in fragments module

--- Fragments/fragment-practice/src/App.jsx
+++ Fragments/fragment-practice/src/App.jsx
@@ -3,17 +3,19 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  // let itemList = ["first", "second", "third", "fourth", "fifth"];
-  let itemList = [];
+  let itemList = ["first", "second", "third", "fourth", "fifth"];
+  // let itemList = [];
   // if (itemList.length === 0) {
   //   return <h2>Not Items here</h2>;
   // }
+  let emptyMessage =
+    itemList.length === 0 ? (
+      <h2 className="bg-danger">Not Items here</h2>
+    ) : null; //adding turnary operation for condition
   return (
     <React.Fragment>
       <h1>Heaalthy Food</h1>
-      {itemList.length === 0 ? (
-        <h2 className="bg-danger">Not Items here</h2> //Adding turnary operation
-      ) : null}
+      {emptyMessage}
       <ul className="list-group">
         {itemList.map((item) => (
           <li key={item} className="list-group-item">
